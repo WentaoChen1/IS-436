9e56466c18960ed857581bd8d5eff52087f0e955
WentaoChen1 <124645965+WentaoChen1@users.noreply.github.com>
2023-04-30 21:43:46 -0400

    Update login.php

--- login.php
+++ login.php
@@ -20,16 +20,5 @@ $result = $conn->query($sql);
 // Mysql_num_row is counting table row
 $count=$result->num_rows;
 
-// If result matched $username and $password, table row must be 1 row
-if($count==1){
-    session_start();
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-	
-	header("Location: page1.php"); /* Redirect browser */
-	}
-else
-	{
-	header("Location: index.html"); /* Redirect browser */
-exit();
-}
+
+?>
